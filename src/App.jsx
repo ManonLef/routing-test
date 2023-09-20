16d8b5af44ec11b925681206394861d8721f3bac
Manon <manonlefkos@gmail.com>
2023-09-20 11:21:00 +0200

    add profile component and link to profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-
-function App() {
-  return <div>hi</div>
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from the main page</h1>
+      <p>some example links</p>
+      <nav>
+        <ul>
+          <li>
+            <a href="profile">yo profile</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
 
 export default App
---
+++ src/Profile.jsx
@@ -0,0 +1,9 @@
+const Profile = () => {
+  return (
+    <div>
+      <h1>Hello from profile page!</h1>
+      <p>So how are you doing?</p>
+    </div>
+  );
+};
+export default Profile
