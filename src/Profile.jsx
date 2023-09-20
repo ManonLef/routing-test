54d421e6442abe8cfe944e825e3b45c28a45c229
Manon <manonlefkos@gmail.com>
2023-09-20 13:57:16 +0200

    add Outlet for child components

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+
 const Profile = () => {
   return (
     <div>
       <h1>Hello from profile page!</h1>
       <p>So how are you doing?</p>
+      <hr />
+      <h2>The profile you visited is here:</h2>
+      <Outlet />
     </div>
   );
 };
-export default Profile
+export default Profile;
